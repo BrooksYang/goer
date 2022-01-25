64b6f675f26d4914f58e409b60194f41393d76d3
Brooks <brooksyang@outlook.com>
2022-01-25 20:58:24 +0800

    fix - docs

--- cmd/root.go
+++ cmd/root.go
@@ -31,7 +31,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.goer.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is ./config.toml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
