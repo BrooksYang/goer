4d5a15eedf79498904d9550c6cfa9cc03f668f6f
Brooks <brooksyang@outlook.com>
2022-02-05 00:11:05 +0800

    add - custom gorm logger

--- bootstrap/database.go
+++ bootstrap/database.go
@@ -2,12 +2,15 @@ package bootstrap
 
 import (
 	"log"
+	"os"
+	"time"
 
 	"goer/global"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var dbConfig gorm.Dialector
@@ -23,8 +26,27 @@ func Gorm() *gorm.DB {
 		GormMysqlConfig()
 	}
 
+	// Log level
+	logLevel := logger.Warn
+	if global.Config.App.IsProduction() {
+		logLevel = logger.Error
+	}
+
+	// Custom logger
+	customLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer（日志输出的目标，前缀和日志包含的内容——译者注）
+		logger.Config{
+			SlowThreshold:             500 * time.Millisecond, // 慢 SQL 阈值
+			LogLevel:                  logLevel,               // 日志级别
+			IgnoreRecordNotFoundError: true,                   // 忽略ErrRecordNotFound（记录未找到）错误
+			Colorful:                  true,                   // 禁用彩色打印
+		},
+	)
+
+	// Open DB
 	db, err := gorm.Open(dbConfig, &gorm.Config{
 		DisableForeignKeyConstraintWhenMigrating: true,
+		Logger:                                   customLogger,
 	})
 	if err != nil {
 		log.Println(err)
