ee53fc464d4d25ef3b981c2f869c55a39ddc5ee6
Brooks <brooksyang@outlook.com>
2022-02-04 15:00:39 +0800

    opt - limiter

--- bootstrap/route.go
+++ bootstrap/route.go
@@ -33,7 +33,7 @@ func SetupRouter() *gin.Engine {
 func registerGlobalMiddleWare(router *gin.Engine) {
 	router.Use(
 		middleware.Cors(),             // 允许跨域请求
-		middleware.LimitMethod("2-S"), // 限制请求方法，GET 不限制，其它每秒2次
+		middleware.LimitMethod("1-S"), // 限制请求方法，GET 不限制，其它每秒1次
 		middleware.LimitIP("10-S"),    // 限制 IP 请求，每秒10次
 	)
 }
