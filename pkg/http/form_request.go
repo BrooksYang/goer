dba2ae3e4345537816208f2c01343c09eaa403a5
Brooks <brooksyang@outlook.com>
2022-02-08 15:07:13 +0800

    opt - validation parse errors

--- pkg/http/form_request.go
+++ pkg/http/form_request.go
@@ -1,11 +1,11 @@
 package http
 
 import (
-	"goer/global/errno"
 	"goer/pkg/response"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"github.com/iancoleman/strcase"
 )
 
 type FormRequest interface {
@@ -26,16 +26,22 @@ func ParseError(request FormRequest, err error) string {
 	return "Parameter error"
 }
 
-func ParseErrors(request FormRequest, err error) map[string]string {
-	data := map[string]string{}
+func ParseErrors(request FormRequest, err error) map[string][]string {
+	data := map[string][]string{}
 
 	for _, v := range err.(validator.ValidationErrors) {
+		errField := strcase.ToSnake(v.Field())
+		errMsg := v.Error()
+
 		if message, exist := request.Messages()[v.Field()+"."+v.Tag()]; exist {
-			data[v.Field()] = message
-			continue
+			errMsg = message
 		}
 
-		data[v.Field()] = v.Error()
+		if _, ok := data[errField]; ok {
+			data[errField] = append(data[errField], errMsg)
+		} else {
+			data[errField] = []string{errMsg}
+		}
 	}
 
 	return data
@@ -43,7 +49,7 @@ func ParseErrors(request FormRequest, err error) map[string]string {
 
 func Validate(c *gin.Context, request FormRequest) bool {
 	if err := c.ShouldBind(request); err != nil {
-		response.FailWithMsg(c, errno.ValidationError.Code, ParseError(request, err))
+		response.ValidationError(c, ParseErrors(request, err))
 		return false
 	}
 
