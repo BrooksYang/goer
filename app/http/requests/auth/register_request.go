5a4aca23d7894edef2f7e084e5f50600938f8dd3
Brooks <brooksyang@outlook.com>
2022-02-08 15:23:44 +0800

    fix - register request email validation

--- app/http/requests/auth/register_request.go
+++ app/http/requests/auth/register_request.go
@@ -3,9 +3,9 @@ package authRequest
 import "goer/pkg/http"
 
 type RegisterRequest struct {
-	Type       int    `form:"type" binding:"required,eq=1|eq=2"` // 1-邮箱注册，2-手机注册
+	Type       int    `form:"type" binding:"required,oneof=1 2"` // 1-邮箱注册，2-手机注册
 	Name       string `form:"name"`
-	Email      string `form:"email" binding:"required_if=Type 1,email"`
+	Email      string `form:"email" binding:"required_if=Type 1,omitempty,email"`
 	Phone      string `form:"phone" binding:"required_if=Type 2"`
 	Password   string `form:"password" binding:"required,min=6"`
 	ReferralId uint   `form:"referral_id"`
@@ -14,7 +14,7 @@ type RegisterRequest struct {
 func (req RegisterRequest) Messages() http.ValidatorMessages {
 	return http.ValidatorMessages{
 		"Type.required":       "Type is required",
-		"Type.eq=1|eq=2":      "Type is invalid",
+		"Type.oneof":          "Type is invalid",
 		"Email.required_if":   "Email is required",
 		"Email.email":         "Email is invalid",
 		"Phone.required_if":   "Phone is required",
