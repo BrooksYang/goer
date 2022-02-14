a515117feeb36c8ca2351bb2040f55a630436ba7
Brooks <brooksyang@outlook.com>
2022-02-14 15:29:32 +0800

    opt - swag

--- routes/swag.go
+++ routes/swag.go
@@ -4,28 +4,31 @@ import (
 	"goer/docs"
 
 	"github.com/gin-gonic/gin"
-	swaggerFiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/files"
+	"github.com/swaggo/gin-swagger"
 )
 
-// @title           API Docs
-// @version         1.0
-
-// @host      localhost:3000
-// @BasePath  /
-
+// MapSwagRoutes
+// @title                       API Docs
+// @version                     1.0
+// @host                        localhost:3000
+// @BasePath                    /
 // @securityDefinitions.apikey  Bearer
 // @in                          header
 // @name                        Authorization
 func MapSwagRoutes(r *gin.Engine) {
 	// swagger info
-	docs.SwaggerInfo.Title = "API Docs"
-	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo_swagger.Title = "API Docs"
+	docs.SwaggerInfo_swagger.Version = "1.0"
 	// docs.SwaggerInfo.Host = "localhost:3000"
 	// docs.SwaggerInfo.BasePath = "/"
 	// docs.SwaggerInfo.Schemes = []string{"http", "https"}
 
 	// use ginSwagger middleware to serve the API docs
 	// DefaultModelsExpandDepth: set -1 to hide models below
-	r.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, ginSwagger.DefaultModelsExpandDepth(-1)))
+	r.GET("/docs/*any", ginSwagger.WrapHandler(
+		swaggerFiles.Handler,
+		ginSwagger.DefaultModelsExpandDepth(-1),
+		ginSwagger.PersistAuthorization(true),
+	))
 }
